0fdfcfd0a35ca3124b39186cc0181c441aa87209
Juan Rey <email@juanrey.net>
2025-05-02 20:24:52 +0200

    PTypes mirror

--- src/main.cpp
+++ src/main.cpp
@@ -42,7 +42,7 @@ int main( int argc, char * argv[] )
 
   printf( "\n%s - %s %s (%s)\n", APP_SHORT_NAME, APP_FULL_NAME, APP_VERSION, APP_WEB );
   printf( "(C) %s\n", APP_COPYRIGHT );
-  printf( "%s uses Hovik Melikyan's C++ Portable Types Library (PTypes) (http://www.melikyan.com/ptypes/)\n", APP_SHORT_NAME );
+  printf( "%s uses Hovik Melikyan's C++ Portable Types Library (PTypes) (mirror https://resolutive.com/ptypes/)\n", APP_SHORT_NAME );
   printf( "Usage: %s [%s path] [(%s)|%s]\n       %s [%s|%s|%s]\n\n", APP_FILE, SET_CONFIG_FILE_PATH, RUN_AS_SERVICE, RUN_IN_CONSOLE_TEXT, APP_FILE, SHOW_LICENSE, INSTALL_SERVICE_TEXT, UNINSTALL_SERVICE_TEXT );
 
 #ifdef DEBUG     
